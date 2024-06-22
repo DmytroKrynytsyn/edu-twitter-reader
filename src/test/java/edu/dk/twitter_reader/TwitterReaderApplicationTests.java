590571cee089c4acae782bf8652ca99695aae1bf
DmytroKrynytsyn <dmitry.krinitsyn@gmail.com>
2024-06-22 21:19:43 +0200

    initial

---
+++ src/test/java/edu/dk/twitter_reader/TwitterReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dk.twitter_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
